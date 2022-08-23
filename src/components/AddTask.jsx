c23c21142bc694c7c206f53f012bb71c27fdb87d
noobchirag69 <chiragchakraborty48@gmail.com>
2022-08-23 17:51:50 +0530

    Done!

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -21,11 +21,11 @@ const AddTask = ({ onAdd }) => {
     <form className='add-form' onSubmit={onSubmit}>
         <div className='form-control'>
             <label>Task</label>
-            <input type="text" placeholder="Add Task" value={text} onChange={(e) => setText(e.target.value)} />
+            <input type="text" placeholder="Add Task" value={text} onChange={(e) => setText(e.target.value)} required/>
         </div>
         <div className='form-control'>
             <label>Day & Time</label>
-            <input type="text" placeholder="Add Day & Time" value={day} onChange={(e) => setDay(e.target.value)} />
+            <input type="text" placeholder="Add Day & Time" value={day} onChange={(e) => setDay(e.target.value)} required/>
         </div>
         <div className='form-control-check'>
             <label>Set Reminder</label>
